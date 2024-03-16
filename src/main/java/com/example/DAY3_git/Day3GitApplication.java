42eca4461cb4e4a1bf7dd2049c425558fb7c1fe8
Sehan Madushanka <43729924+sehanmadu95@users.noreply.github.com>
2024-03-16 15:29:14 +0530

    initial commit

---
+++ src/main/java/com/example/DAY3_git/Day3GitApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAY3_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3GitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DAY3_git/Day3GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAY3_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
